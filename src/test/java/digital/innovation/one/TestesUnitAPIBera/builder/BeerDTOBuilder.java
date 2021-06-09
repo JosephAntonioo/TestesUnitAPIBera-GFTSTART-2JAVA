4a6ba993d23a56225e8901a3c9d84246f33935fc
JosephAntonioo <jose-tof@hotmail.com>
2021-06-09 01:27:14 -0300

    Builder - Popula um objeto Beer com os dados da cerveja BudWeiser,"Buduvieser" para os mais íntimos.

--- src/test/java/digital/innovation/one/TestesUnitAPIBera/builder/BeerDTOBuilder.java
+++ src/test/java/digital/innovation/one/TestesUnitAPIBera/builder/BeerDTOBuilder.java
@@ -1,4 +1,36 @@
 package digital.innovation.one.TestesUnitAPIBera.builder;
 
+import digital.innovation.one.TestesUnitAPIBera.dto.BeerDTO;
+import digital.innovation.one.TestesUnitAPIBera.enums.BeerType;
+import lombok.Builder;
+
+@Builder
 public class BeerDTOBuilder {
+
+    @Builder.Default
+    private Long id = 1L;
+
+    @Builder.Default
+    private String name = "Budweiser";
+
+    @Builder.Default
+    private String brand = "AB InBev";
+
+    @Builder.Default
+    private int max = 50;
+
+    @Builder.Default
+    private int quantity = 10;
+
+    @Builder.Default
+    private BeerType type = BeerType.LAGER;
+
+    public BeerDTO toBeerDTO(){
+        return new BeerDTO(id,
+                name,
+                brand,
+                max,
+                quantity,
+                type);
+    }
 }
